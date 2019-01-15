649820e042224469a6ba13705711f975bdbfbf24
Avegno <luis.horacio.avegno@accenture.com>
2019-01-15 00:22:46 -0300

    All the game

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 function random_items(k, items) {
-    """ Selects `k` random items without repeats from `items` """
+
+  // Selects `k` random items without repeats from `items`
 
     var selected = [];
     for (var i = 0; i < k; i++) {
@@ -36,14 +37,37 @@ class Memtest {
       this.level = new Level(1)
       this.img_holders = [$("#image1"), $("#image2"),
                           $("#image3"), $("#image4")]
-
+      this.responses = [];
       this.start();
     }
 
     start() {
+      this.responses = [];
       for (var i = 0; i < this.level.number; i++) {
-        this.img_holders[i].src = "images/" + this.level.symbols[i] + ".jpg";
+        this.img_holders[i].attr('src', "images/" + this.level.symbols[i] + ".jpg");
       }
+    }
+
+    submit(){
+      var in_text = $("input").val();
+      in_text = in_text.toLowerCase();
+      if (in_text == this.level.symbols[this.responses.length]) {
+          // Correct answer
+          this.responses.push(in_text);
+          if (this.responses.length == this.level.number) {
+            // End game
+            if (this.level.number == 4) {
+              
+            }
+            // End of level
+            this.level = new Level(this.level.number + 1);
+            this.start();
+          }
+      } else {
+        // Incorrect answer
+        // TODO: END LEVEL
+      }
+      $("input").val("");
     }
 };
 
@@ -53,6 +77,14 @@ $(document).ready(function() {
     console.log("Loaded");
 
     var memtest = new Memtest();
+
+    $(".btn-success").click(memtest.submit.bind(memtest));
+
+    $("input").keyup(function(event) {
+      if (event.keyCode == 13) {
+        memtest.submit();
+      }
+    });
 
     if (TEST) {
         console.log("Starting tests...");
